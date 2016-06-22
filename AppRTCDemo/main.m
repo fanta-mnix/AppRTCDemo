bb370ca176aafc3e72342454264754dad45f9105
Rafael Fantini da Costa <rafael.fantini@movile.com>
2016-06-22 10:48:55 -0300

    Initial commit

---
+++ AppRTCDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AppRTCDemo
+//
+//  Created by Rafael Fantini on 10/21/15.
+//  Copyright © 2015 Movile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ARDAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ARDAppDelegate class]));
+    }
+}
